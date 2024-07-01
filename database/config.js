7adfe96d7d8ca998fa27c49cd85f9d9496621189
ZulkarnainMopili <ananmopili37@gmail.com>
2024-07-01 13:56:58 +0800

    Update config.js

--- database/config.js
+++ database/config.js
@@ -1,1 +1,20 @@
+const mysql = require('mysql2');
 
+const config = {
+    host: 'localhost',    // Ganti dengan host database Anda
+    user: 'root',         // Ganti dengan user database Anda
+    password: 'password', // Ganti dengan password database Anda
+    database: 'ecommerce' // Nama database yang telah dibuat
+};
+
+const connection = mysql.createConnection(config);
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database.');
+});
+
+module.exports = connection;
